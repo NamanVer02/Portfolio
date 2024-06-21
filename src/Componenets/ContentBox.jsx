8005ee17c185df9dc22d63ee6b14b5ad4ddee222
NamanVer02 <namanver.2002@gmail.com>
2024-06-21 11:33:18 +0530

    --

--- src/Componenets/ContentBox.jsx
+++ src/Componenets/ContentBox.jsx
@@ -1,22 +1,33 @@
 import React from 'react'
-import { motion, spring } from 'framer-motion'
+import { color, motion, spring } from 'framer-motion'
 
-function ContentBox({ height, width, aspectRatio, title, subHeading }) {
+function ContentBox({ height, width, aspectRatio, title, subHeading, backgroundURL }) {
   const style = {
     height: height,
     width: width,
     aspectRatio: aspectRatio,
+    color: 'black',
+    backgroundImage: `linear-gradient( rgba(0, 0, 0, 0.0), rgba(0, 0, 0, 0.0) )`
+  }
+
+  const hoverStyle = {
+    scale: 1.05,
+    backgroundImage: `linear-gradient( rgba(0, 0, 0, 0.11), rgba(0, 0, 0, 0.7) ), url(${backgroundURL})`, // Replace with your image URL
+    backgroundSize: 'cover', // Ensure the image covers the entire box
+    backgroundPosition: 'center', // Center the image
+    border: 'none', // Remove border on hover
+    color: 'white' // Change text color to white
   }
 
   return (
     <motion.div 
-      class="box"
+      className="box"
       style={style}
-      whileHover={{scale: 1.05, backgroundColor: 'red', color: 'white'}}
-      transition={{type: 'spring'}}
+      whileHover={hoverStyle}
+      transition={{type: 'spring', border: {duration: 0}, backgroundImage: {duration: 0}}}
     >
-        <h1 class="title">{title || "Title"}</h1>
-        <h2 class="subheading">{subHeading || "Subheading"}</h2>
+        <h1 className="title">{title || "Title"}</h1>
+        <h2 className="subheading">{subHeading || "Subheading"}</h2>
     </motion.div>
   )
 }
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -4,12 +4,28 @@ import Navbar from '../Componenets/Navbar'
 import './Contact.css'
 
 function Contact() {
+  let contactList = [
+    {
+      title: 'Email',
+      content: 'namanver.2002@gmail.com',
+      link: 'www.google.com',
+      background: 'https://st5.depositphotos.com/73964430/64907/i/450/depositphotos_649078456-stock-illustration-water-color-oil-painting-fine.jpg'
+    }
+  ];
+
   return (
     <div className='contact'>
         <Navbar></Navbar>
         <div className="content">
             <h1>Contact Me</h1>
             <div className="contact-grid">
+              {contactList.map((item) => (
+                <ContentBox 
+                  title={item.title}
+                  subHeading={item.content}
+                  backgroundURL={item.background}
+                ></ContentBox>
+              ))}
                 <ContentBox title="Email" subHeading="namanver.2002@gmail.com"></ContentBox>
                 <ContentBox title="LinkedIn" subHeading="namanver.02"></ContentBox>
                 <ContentBox title="Github" subHeading="NamanVer02"></ContentBox>
